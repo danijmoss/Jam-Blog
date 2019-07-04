574133b9da85a9ee77b79842e37c5dd7913b2987
Danielle Moss <dani.code.camp@gmail.com>
2019-07-04 15:25:23 -0400

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    
+    <header class="header">
+      <h1>Gridsome Blog</h1>
+      <p>A simple blog designed with Gridsome</p>
+    </header>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  }
+};
+</script>
+
+<style>
+.header {
+  font-family: "Stylish";
+  font-size: 35px;
+  text-align: center;
+  line-height: 20px;
+  padding: 0.7em;
+}
+
+.header p {
+  font-weight: 200;
+}
+</style>
